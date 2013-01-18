1ab4761e21cbe377d34bd93a2d99ba8bc7fbb9a2
Jose F. Romaniello <jfromaniello@gmail.com>
2013-01-18 18:53:38 -0300

    implement server-side logic for company-wide visibility

--- test/docs.tests.js
+++ test/docs.tests.js
@@ -44,7 +44,13 @@ describe('docs', function () {
                   email: 'baz@bar.com',
                   type:  'can view'
                 }
-              ]
+              ],
+              visibility: {
+                companies: {
+                  'kluglabs': 'can view',
+                  'microsoft': 'can edit'
+                }
+              }
             }, function (err, res){
               if(err) return cb(err);
               test.docId = res[0]._id;
@@ -106,7 +112,7 @@ describe('docs', function () {
       });
     });
 
-    it('should return error if the user is collaborator but "can view"', function (done) {
+    it('should return error if the user is collaborator but only "can view"', function (done) {
       docs.getForEdit({
         _id: ObjectID.createPk(),
         emails: [{
@@ -118,8 +124,43 @@ describe('docs', function () {
       });
     });
 
+    it('should return the doc when the user belongs to a company that can edit', function (done) {
+      docs.getForEdit({
+        _id: ObjectID.createPk(),
+        emails: [{
+          type: 'foo', value: 'baz@microsoft.com'
+        }],
+        identities: [{ connection: 'microsoft'}]
+      }, test.docId, function (err, doc) {
+        if(err) return done(err);
+        doc._id.toString().should.eql(test.docId.toString());
+        done();
+      });
+    });
   });
 
+  
+  describe('user can edit doc', function () {
+    it('should return true for company-wide editable docs', function(){
+       var user = {
+        emails: [],
+        identities: [{
+          connection: 'kluglabs'
+        }]
+       };
+       var doc = {
+        owner: 'faa',
+        collaborators: [],
+        visibility: {
+          companies: {
+            kluglabs: 'can edit'
+          }
+        }
+       };
+       docs.userCanEditDoc(user, doc)
+          .should.be.true;
+    });
+  });
 
 
   describe('get for view' , function () {
@@ -164,20 +205,80 @@ describe('docs', function () {
       });
     });
 
+    it('should return the doc when the user belongs to a company that can edit', function (done) {
+      docs.getForView({
+        _id: ObjectID.createPk(),
+        emails: [{
+          type: 'foo', value: 'baz@microsoft.com'
+        }],
+        identities: [{ connection: 'microsoft'}]
+      }, test.docId, function (err, doc) {
+        if(err) return done(err);
+        doc._id.toString().should.eql(test.docId.toString());
+        done();
+      });
+    });
+
+    it('should return the doc when the user belongs to a company that can view', function (done) {
+      docs.getForView({
+        _id: ObjectID.createPk(),
+        emails: [{
+          type: 'foo', value: 'baz@microsoft.com'
+        }],
+        identities: [{ connection: 'kluglabs'}]
+      }, test.docId, function (err, doc) {
+        if(err) return done(err);
+        doc._id.toString().should.eql(test.docId.toString());
+        done();
+      });
+    });
   });
 
   describe('get all' , function () {
-    it('should return canEdit true/false', function (done) {
+    it('should return docs user can read or view', function (done) {
       docs.getAll({
         _id: ObjectID.createPk(),
         emails: [{
           type: 'foo', value: 'foo@bar.com'
         }] 
       }, function (err, docs) {
-        docs[0].canEdit.should.be.true;
+        if(err) return done(err);
         docs[0].name.should.eql('expenses');
-        docs[1].canEdit.should.be.false;
         docs[1].name.should.eql('expenses 2');
+        done();
+      });
+    });
+
+    it('should return docs with company-wide visibility', function (done) {
+      var user = {
+        identities: [{
+          connection: 'kluglabs'
+        }],
+        emails: [{
+          value: 'bob@kluglabs'
+        }]
+      };
+
+      docs.getAll(user, function (err, docs) {
+        if (err) return done(err);
+        docs[0].name.should.eql('expenses');
+        done();
+      });
+    });
+
+    it('should not return docs with company-wide visibility but user from other company', function (done) {
+      var user = {
+        identities: [{
+          connection: 'qraftlabs'
+        }],
+        emails: [{
+          value: 'bob@kluglabs'
+        }]
+      };
+
+      docs.getAll(user, function (err, docs) {
+        if (err) return done(err);
+        docs.length.should.eql(0);
         done();
       });
     });
