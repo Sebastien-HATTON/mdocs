bf8a1f2fd35c5842ff7d88f480929a3f4c016147
Jose F. Romaniello <jfromaniello@gmail.com>
2013-01-18 10:24:18 -0300

    add provisioning popup & minor fixes

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,22 @@
 define(function (require) {
   require('bootstrap');
+  
   $('#sign-in').click(function(e){
     e.preventDefault();
     window.Auth0.signIn();
+  });
+
+  $('#create-company').click(function (e) {
+    
+    e.preventDefault();
+
+    window.Auth0.showProvisioning(function(data, callback) {
+       $.post('/provisioning', {
+          data: data, 
+          success: function(result) { 
+            callback({worked: true, provisioning_ticket_url: 'https://markdio.auth0.com/p/google-apps/c6w3zsEC'});  
+          }
+        });
+    });
   });
 });
