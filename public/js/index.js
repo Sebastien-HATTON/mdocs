64fe689f458653fb13f8c48e2fda486998448d81
Jose F. Romaniello <jfromaniello@gmail.com>
2013-01-19 10:24:01 -0300

    integrated provisioning

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 define(function (require) {
   require('bootstrap');
   require('auth0');
+  var request = require('reqwest');
+
   $('#sign-in, .sign-in').click(function(e){
     e.preventDefault();
     window.Auth0.signIn();
@@ -10,12 +12,18 @@ define(function (require) {
     
     e.preventDefault();
       window.Auth0.showProvisioning(function(data, callback) {
-        $.post('/provisioning', {
-          data: data, 
-          success: function(result) { 
-            callback({worked: true, provisioning_ticket_url: 'https://markdio.auth0.com/p/google-apps/c6w3zsEC'});  
-          }
-        });
+
+        request({
+            url:          '/provisioning',
+            method:       'post',
+            contentType:  'application/json',
+            data:         JSON.stringify(data),
+            type:         'json',
+            success: function (r) {
+              callback(r); 
+            }
+          });
+
       });
   });
 });
